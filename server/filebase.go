99e3af016bb45cbd61aadb2d7822a6d8797dd486
BH Kim <shoebill.k@gmail.com>
2018-08-27 23:24:45 +0900

    filebase 일부 구현

--- server/filebase.go
+++ server/filebase.go
@@ -1,1 +1,27 @@
 package server
+
+import (
+	"errors"
+	"io"
+)
+
+// FileBase implements Provider interface using file system.
+type FileBase struct {
+	root string
+}
+
+// NewFileBase ...
+func NewFileBase(root string) *FileBase {
+	return &FileBase{root}
+}
+
+// Open return ReadCloser interface for read the contents.
+func (fb *FileBase) Open(ID string) (io.ReadCloser, error) {
+	return nil, errors.New("not implemented")
+}
+
+// Create return WriteCloser interface for write the contents.
+func (fb *FileBase) Create(ID string) (io.WriteCloser, error) {
+	return nil, errors.New("not implemented")
+
+}
