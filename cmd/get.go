e9fcc6868203f628c03b43ca5edc8893cc085387
BH Kim <shoebill.k@gmail.com>
2018-09-05 22:41:50 +0900

    Get 구현

--- cmd/get.go
+++ cmd/get.go
@@ -1,50 +1,74 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"log"
 
+	"github.com/shoebillk/sbs/blob"
 	"github.com/spf13/cobra"
+	"google.golang.org/grpc"
 )
 
 // getCmd represents the get command
 var getCmd = &cobra.Command{
 	Use:   "get",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "get blob data from server",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("get called")
+		log.Println("get called")
+		host, err := cmd.Flags().GetString("host")
+		if err != nil {
+			log.Fatal(err)
+		}
+		port, err := cmd.Flags().GetInt("port")
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		Id := args[0]
+		log.Printf("ID : %s\n", Id)
+
+		target := fmt.Sprintf("%s:%d", host, port)
+		log.Printf("Server : %s", target)
+		conn, err := grpc.Dial(target, grpc.WithInsecure())
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer conn.Close()
+
+		client := blob.NewBlobServiceClient(conn)
+
+		req := blob.GetRequest{Id: Id}
+
+		var callopts []grpc.CallOption
+		getClient, err := client.Get(context.Background(), &req, callopts...)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for {
+			chunk, err := getClient.Recv()
+
+			if err == io.EOF {
+				log.Printf("Done to read")
+				break
+			} else if err != nil {
+				log.Fatal(err)
+				break
+			}
+
+			log.Printf("Read %d", len(chunk.Content))
+			log.Printf("Read %s", chunk.Content)
+
+		}
+
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(getCmd)
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// getCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// getCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	getCmd.Flags().StringP("host", "s", DefaultHost, "Host string of server")
+	getCmd.Flags().IntP("port", "p", DefaultPort, "Port number of server")
 }
